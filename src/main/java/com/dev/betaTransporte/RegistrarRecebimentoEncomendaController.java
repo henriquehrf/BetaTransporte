e9b642d81254e0c254305f8733494ab401661f25
Henrique R. Firmino <henrique_rfirmino@hotmail.com>
2018-02-14 14:19:04 -0300

    Implementação parcial da tela registro de recebimento de encomenda - WIP

--- src/main/java/com/dev/betaTransporte/RegistrarRecebimentoEncomendaController.java
+++ src/main/java/com/dev/betaTransporte/RegistrarRecebimentoEncomendaController.java
@@ -7,7 +7,16 @@ package com.dev.betaTransporte;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.KeyEvent;
 
 /**
  * FXML Controller class
@@ -16,9 +25,69 @@ import javafx.fxml.Initializable;
  */
 public class RegistrarRecebimentoEncomendaController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+  
+    
+    @FXML
+    private TableView<?> tbvPrincipal;
+
+    @FXML
+    private TableColumn<?, ?> tbcCodigo;
+
+    @FXML
+    private ToggleGroup filter;
+
+    @FXML
+    private TableColumn<?, ?> TbcCliente;
+
+    @FXML
+    private TableColumn<?, ?> tbcCPFCNPJ;
+
+    @FXML
+    private TextField txtPesquisa;
+
+    @FXML
+    private Button btnSelecionar;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private RadioButton rdbCodigo;
+
+    @FXML
+    private RadioButton rdbNome;
+
+    @FXML
+    private TableColumn<?, ?> tbcNF;
+
+    @FXML
+    private RadioButton rdbCpfCnpj;
+
+    @FXML
+    void btnSelecionarOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSelecionarOnKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void btnVoltarOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnVoltarOnKeyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void txtPesquisaOnKeyPressed(KeyEvent event) {
+
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
